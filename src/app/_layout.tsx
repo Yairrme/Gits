927c188756c4790185d6f6bb74d18296e5969f50
Yairrme <agustinmelinguer@hotmail.com>
2026-08-29 22:18:26 -0300

    feat: setup clean login screen

---
+++ src/app/_layout.tsx
@@ -0,0 +1,16 @@
+import { DarkTheme, DefaultTheme, ThemeProvider } from 'expo-router';
+import { Stack } from 'expo-router';
+import * as SplashScreen from 'expo-splash-screen';
+import { useColorScheme } from 'react-native';
+
+SplashScreen.preventAutoHideAsync();
+
+export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  return (
+    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
+      {/* Esto asegura que TODAS las pantallas del proyecto no tengan header por defecto */}
+      <Stack screenOptions={{ headerShown: false }} />
+    </ThemeProvider>
+  );
+}
